6228235d1c23ac68fa16af392d53c35e1f6408ba
Luxe17 <hanson1717@live.com>
2023-08-25 10:51:43 -0700

    Added Video component

---
+++ src/components/Video.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Year {
+    id: string,
+    link: string
+}
+const props = defineProps<{
+    year: Year
+}>()
+
+
+</script>
+
+<template>
+    <iframe
+    width="100%" 
+    height="100%" 
+    v-bind:src="props.year.link"
+    title="YouTube video player" 
+    frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+</template>
