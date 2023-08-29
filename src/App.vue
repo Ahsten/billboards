e2419000cf482c08dce99aa32567961c5ddb2e80
Luxe17 <hanson1717@live.com>
2023-08-29 09:33:12 -0700

    Implemented players component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import Video from './components/Video.vue';
 import data from "./assets/data.json";
-import { ref } from 'vue';
+import Players from "./components/Players.vue"
+import { ref, watch } from 'vue';
 
 interface Year {
   id: string,
@@ -11,30 +12,33 @@ interface Year {
 const years = data.years;
 const selected = ref<Year>({"id": "2022", "link": "https://www.youtube.com/embed/Q2VfUqpLYLo?si=ZnaOwZkHS2xXYzRh"});
 
+watch(selected, (newSelected) => {
+  console.log(newSelected)
+});
 </script>
 
 <template>
-  <h1>Year: {{ selected?.id }}</h1>
-    <select v-model="selected">
-        <option disabled value="">Please select a year</option>
-        <option v-for="year in years" :key="year.id" :value="year.link">
-            {{ year.id }}
-        </option>
-    </select>
-  <Video :year="selected"/>
+  <div class="drawer drawer-open overflow-hidden">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content object-scale-down">
+      <div class="navbar w-full shadow-md sticky top-0 z-30 bg-base-100 flex justify-between">
+        <h2 class="text-3xl text-accent font-bold">Year: {{ selected?.id }}</h2>
+        <select v-model="selected" class="select select-sm select-accent">
+            <option v-for="year in years" :key="year.id" :value="year">
+                {{ year.id }}
+            </option>
+        </select>
+      </div>
+      <Video :year="selected"/>
+    </div>
+    <div class="drawer-side z-40">
+      <aside class="w-80 h-screen">
+        <header class="bg-base-100 flex sticky top-0 z-20 items-center justify-center shadow-md px-4 px-2 h-16">
+          <h1 class="text-5xl text-accent font-bold">Billboards</h1>
+        </header>
+        <div class="h-4"></div>
+        <Players/>
+      </aside>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
